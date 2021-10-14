5ea41e56b3d4e9a20c25da55a5acb68bf962c80b
Gail Eddy <gaileddy68@gmail.com>
2021-10-14 07:39:58 -0500

    adds about method and path

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -2,4 +2,8 @@ class WelcomesController < ApplicationController
   def hello_method
     render json: { message: "Hello from welcomes controller!" }
   end
+
+  def about_method
+    render json: { message: "My favorite computing language is Ruby!" }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get "/welcome_path", controller: "welcomes", action: "hello_method"
+  get "/welcome_path", controller: "welcomes", action: "about_method"
 end
